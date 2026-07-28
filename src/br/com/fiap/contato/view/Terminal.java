cb6e9c17804fc6f2309784ad3c8e580ac8d63481
MatheusC0625 <stsycosta@gmail.com>
2026-07-28 18:45:20 -0300

    Adicionando os packages, classes e seus atributos

---
+++ src/br/com/fiap/contato/view/Terminal.java
@@ -0,0 +1,12 @@
+package br.com.fiap.contato.view;
+import javax.swing.*;
+
+public class Terminal {
+    public static void main(String[] args){
+        String nome = JOptionPane.showInputDialog("Digite seu nome: ");
+        JOptionPane.showMessageDialog(null, "Olá, " + nome + "!");
+
+        String numero = JOptionPane.showInputDialog("Digite o número do contato: ");
+        JOptionPane.showMessageDialog(null, "Número confirmado!");
+    }
+}
